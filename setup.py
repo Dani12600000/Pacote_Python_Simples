c1f7539cdcb717d33417d3bdd01b32b68d0abb3e
Dani12600000 <dp709020@gmail.com>
2024-05-16 16:37:20 +0100

    update_aiai

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import os
 from setuptools import setup, find_packages
 setup(
     name="DanielPereira",
-    version="1.5",
+    version="1.6",
     description="Um codigo incrivel, FUNCIONAL, feito pelo grandioso Daniel Pereira :)",
     packages=find_packages(),
     include_package_data=True
