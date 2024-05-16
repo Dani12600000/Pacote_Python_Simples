6a10943e65b573b9a6edc8fba3a5b313cbc7c09d
Dani12600000 <dp709020@gmail.com>
2024-05-16 15:27:33 +0100

    update

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+setup(name='DanielPereira',
+      version='1.0',
+      description='Modulo para fazer somas e comprimentar',
+      author='Daniel',
+      author_email='dp709020@gmail.com',
+      url='https://github.com/Dani12600000/Pacote_Python_Simples',
+      py_modules=['DanielPereira'],
+     )
