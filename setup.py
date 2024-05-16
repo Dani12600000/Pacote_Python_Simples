94d36de37b70b81112705bbf2dfb91ae2003a8b6
Dani12600000 <dp709020@gmail.com>
2024-05-16 21:20:49 +0100

    update_completamente_final

--- setup.py
+++ setup.py
@@ -3,8 +3,11 @@ import os
 from setuptools import setup, find_packages
 setup(
     name="DanielPereira",
-    version="2.1",
-    description="Um codigo incrivel, NÃO FUNCIONAL, feito pelo grandioso Daniel Pereira :)",
+    version="2.2",
+    description="Um codigo incrivel, COMPLETAMENTE FUNCIONAL, feito pelo grandioso Daniel Pereira :)",
     packages=find_packages(),
-    include_package_data=True
+    include_package_data=True,
+    author="Daniel Pereira",
+    author_email="dp709020@gmail.com",
+    url="https://github.com/Dani12600000/Pacote_Python_Simples"
 )
