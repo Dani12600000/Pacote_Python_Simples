30c97ff272eae03dc3788f0c33c5d01e5a725045
Dani12600000 <dp709020@gmail.com>
2024-05-16 21:13:41 +0100

    update

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import os
 from setuptools import setup, find_packages
 setup(
     name="DanielPereira",
-    version="2.0",
+    version="2.1",
     description="Um codigo incrivel, NÃO FUNCIONAL, feito pelo grandioso Daniel Pereira :)",
     packages=find_packages(),
     include_package_data=True
