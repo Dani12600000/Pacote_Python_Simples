9b6b37d965510cfb09075d7c6fe8ca49c5a67e11
Dani12600000 <dp709020@gmail.com>
2024-05-16 16:54:44 +0100

    update_fim

--- DanielPereira/__init__.py
+++
@@ -1,1 +0,0 @@
-from DanielPereira.codigo import *
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import os
 from setuptools import setup, find_packages
 setup(
     name="DanielPereira",
-    version="1.7",
+    version="1.8",
     description="Um codigo incrivel, NÃO FUNCIONAL, feito pelo grandioso Daniel Pereira :)",
     packages=find_packages(),
     include_package_data=True
